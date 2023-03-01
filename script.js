f97ad613ae198ddd85dc93bb8ae1bb1bb060131f
Jenny Takahara <jenny.takahara@gmail.com>
2023-02-28 19:24:42 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,119 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruits = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function search(str) {
+  let results = [];
+  for (let fruit of fruits) {
+    if (fruit.toLowerCase().includes(str.toLowerCase())) {
+      results.push(fruit);
+    }
+  }
+  return results;
+}
+
+function searchHandler(e) {
+  const results = search(input.value);
+  showSuggestions(results, input.value);
+}
+
+function showSuggestions(results, inputVal) {
+  suggestions.innerHTML = "";
+  for (let result of results) {
+    let newResult = document.createElement("li");
+    newResult.innerText = result;
+    suggestions.append(newResult);
+  }
+  if (inputVal === "") {
+    suggestions.innerHTML = "";
+  }
+}
+
+function useSuggestion(e) {
+  suggestions.innerHTML = "";
+  input.value = e.target.innerHTML;
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
